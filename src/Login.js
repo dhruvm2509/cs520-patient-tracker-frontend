4a7aa68fd2a45428ce7f99a2613ca76e8ba43437
Matt Lips <mlips@umass.edu>
2023-10-26 19:37:49 -0400

    Added shading background

--- src/Login.js
+++ src/Login.js
@@ -10,50 +10,57 @@ function Login() {
 	};
 
 	return (
-		<div className="Login">
+		<div className="login">
 			<div className="header">
 				Patient Tracker Web App
 			</div>
-			<div>
-				<img src={logo} alt="Patient Tracker logo" class="logo" />
-			</div>
-			<div className="username">
-				<ShortTextField
-					className="username"
-					placeholder="Username"
-					outerText="Username:"
-				/>
-			</div>
-			<div className="password">
-				<ShortTextField
-					className="password"
-					placeholder="Password"
-					outerText="Password:"
-					isPassword="true"
-				/>
+			<div className="container">
+				<div className="shade"></div>
+				<div>
+					<div>
+						<img src={logo} alt="Patient Tracker logo" class="logo" />
+					</div>
+					<div className="username">
+						<ShortTextField
+							className="username"
+							placeholder="Username"
+							outerText="Username:"
+						/>
+					</div>
+					<div className="password">
+						<ShortTextField
+							className="password"
+							placeholder="Password"
+							outerText="Password:"
+							isPassword="true"
+						/>
+					</div>
+
+					<div className="forgot-password">
+						<a href='/forgot-password'><u>Forgot Password?</u></a>
+					</div>
+
+					<div className="login-button">
+						<a href="/login">
+							<PillButton
+								text="Log In"
+								fontSize="22"
+								pixelHeight="50"
+								pixelWidth="200"
+								href="/test1"
+								onClick={handleClick}
+							/>
+						</a>
+
+					</div>
+
+					<div className="sign-up">
+						<a href='/signup'><u>Sign Up</u></a>
+					</div>
+				</div>
+				<div className="shade"></div>
 			</div>
 
-			<div className="forgot-password">
-				<a href='/forgot-password'><u>Forgot Password?</u></a>
-			</div>
-
-			<div className="login-button">
-				<a href="/login">
-					<PillButton
-						text="Log In"
-						fontSize="22"
-						pixelHeight="50"
-						pixelWidth="200"
-						href="/test1"
-						onClick={handleClick}
-					/>
-				</a>
-
-			</div>
-
-			<div className="sign-up">
-				<a href='/signup'><u>Sign Up</u></a>
-			</div>
 		</div>
 	);
 }
