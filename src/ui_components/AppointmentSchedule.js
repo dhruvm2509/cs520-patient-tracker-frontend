12fda247fae6ee9e6f1684a388533e26207005a9
Matt Lips <mlips@umass.edu>
2023-12-14 16:08:47 -0500

    Refactored AppointmentSchedule to be in card form and show doctor information

--- src/ui_components/AppointmentSchedule.js
+++ src/ui_components/AppointmentSchedule.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import PillButton from './PillButton.js';
+import doctorImage from './../resources/DoctorPlaceholder.jpg';
 
 function AppointmentSchedule(props) {
+
+	const appointmentCardStyle = {
+		backgroundColor: props.backgroundColor || '#5142ffff',
+		borderRadius: '20px',
+		fontWeight: 'bold',
+		height: (100 + 70 * Math.ceil(props.appointments.length / 4)) + 'px'
+	};
+
+	const profileImageStyle = {
+		width: '50px',
+		height: '50px',
+		margin: '10px',
+	};
+
+	const mergedStyle = { ...appointmentCardStyle, ...props.style };
+
+	function formatDate(date) {
+		const monthNames = [
+			'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+			'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+		];
+		const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+		const day = date.getDate();
+		const month = monthNames[date.getMonth()];
+
+		return `${dayNames[date.getDay()]}, ${month} ${day}`;
+	}
 
 	function buttonsGrid() {
 		const buttonsGrid = [];
@@ -13,8 +42,8 @@ function AppointmentSchedule(props) {
 				}
 
 				const index = 4 * i + j;
-				const textColor = "white";
-				const backgroundColor = "#8dcc77";
+				const textColor = "black";
+				const backgroundColor = "white";
 
 				buttonsRow.push(
 					<PillButton
@@ -41,8 +70,13 @@ function AppointmentSchedule(props) {
 
 	return (
 		<div>
-			<div className="medium-text medium-margin left-align-text"><b>{props.doctorName} available times:</b></div>
-			{buttonsGrid()}
+			<div style={mergedStyle} className={`row-container ${props.className}`}>
+				<img src={props.imageSource ? props.imageSource : doctorImage} alt='Sick patient placeholder' style={profileImageStyle} className='circle-border' />
+				<div className="left-align-items">
+					<div className="medium-text small-margin white-color">{props.doctorName} | {formatDate(props.date)}</div>
+					{buttonsGrid()}
+				</div>
+			</div>
 		</div>
 	);
 }
